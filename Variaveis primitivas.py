4cde1bf84b1db829e6b6123f41de071972dbe1c3
ronon1 <byurenan@hotmail.com>
2025-02-17 19:51:27 -0300

    first commit git

---
+++ Variaveis primitivas.py
@@ -0,0 +1,12 @@
+idade = 32 # (Int) Número inteiros
+peso = 56.2 # (float) Números decimais
+nome = 'Sharon' # (Str) Caracteres
+ativo = True # (Bool) Valores lógicos
+resultado = None # (None) Ausência de valor
+
+
+print(type(idade))
+print(type(peso))
+print(type(nome))
+print(type(ativo))
+print(type(resultado))
